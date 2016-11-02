836161135dac5e6860df8f3b844eba3de12e3cb3
Jasmi <jasmine.shaheer@gmail.com>
2016-11-02 11:40:05 +0100

    This is my third commit from jas-dev.Now i have added CalculatorAdvancedOperations interface..

---
+++ src/main/java/interfaces/CalculatorAdvancedOperations.java
@@ -0,0 +1,13 @@
+package interfaces;
+
+public interface CalculatorAdvancedOperations {
+	
+	public double sin();
+	public double cos();
+	public double tan();
+	public double percentage();
+	public double squareRoot();
+	public double square();
+	public double oneDividedBy();
+
+}
