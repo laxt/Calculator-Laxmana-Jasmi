f13eca7878e3ae44a8163d1fb852933e59434f4a
Laxmana <laxmana.thapa@gmail.com>
2016-11-02 10:08:54 +0100

    First commit by Laxmana: I just created project and work on little design

---
+++ src/main/java/gui/CalculatorGUI.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+
+public class CalculatorGUI {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CalculatorGUI window = new CalculatorGUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public CalculatorGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+}
