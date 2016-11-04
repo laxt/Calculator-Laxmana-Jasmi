c7992f481ee45f8c33986960aa61938683ca294b
Laxmana <laxmana.thapa@gmail.com>
2016-11-04 10:42:23 +0100

    Now all the functions are done in GUI. Commited from lax-dev

--- src/main/java/gui/CalculatorGUI.java
+++ src/main/java/gui/CalculatorGUI.java
@@ -1,11 +1,17 @@
 package gui;
 
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+
+import otherclasses.MethodsForAdvancedCalculator;
+import otherclasses.MethodsForSimpleCalculations;
+import java.awt.Font;
 
 
 
@@ -27,15 +33,23 @@ public class CalculatorGUI implements ActionListener{
 	private JButton btnMultiply;
 	private JButton btnDivide;
 	private JButton btnPi;
-	private JButton btnClear;
+	private JButton btnEqual;
 	private JButton btnSquareRoot;
 	private JButton btnSquare;
 	private JButton btnLog;
-	private JButton btnAc;
+	private JButton btnDot;
 	private JButton btnSin;
 	private JButton btnCos;
 	private JButton btnTan;
 
+	private  MethodsForSimpleCalculations MSCalc;
+	private String mathInputString;
+	private Object result;
+	private ScriptEngineManager manager = new ScriptEngineManager();
+	private ScriptEngine engine = manager.getEngineByName("js");
+
+	private MethodsForAdvancedCalculator mca = new MethodsForAdvancedCalculator();
+	private JButton btnAC;
 
 
 	/**
@@ -51,16 +65,22 @@ public class CalculatorGUI implements ActionListener{
 	public CalculatorGUI() {
 		CreateUI();
 		addComponentsToFrame();
+		addActionListeners();
 	}
 
 	
 			public void CreateUI() {
 					
 					frame = new JFrame("Calculator-Lax-Jasmi");
+					frame.setResizable(false);
+					frame.setFont(new Font("Dialog", Font.PLAIN, 10));
+					frame.setBounds(100, 100, 421, 254);
+					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 					frame.getContentPane().setLayout(null);
+
 					
 					textDisplay = new JTextField();
-					textDisplay.setBounds(0, 6, 400, 36);
+					textDisplay.setBounds(0, 6, 397, 36);
 					textDisplay.setColumns(10);
 					
 					btn1 = new JButton("0");
@@ -108,8 +128,8 @@ public class CalculatorGUI implements ActionListener{
 					btnPi = new JButton("\u03C0 ");
 					btnPi.setBounds(195, 134, 51, 29);
 					
-					btnClear = new JButton("=");
-					btnClear.setBounds(195, 172, 51, 29);
+					btnEqual = new JButton("=");
+					btnEqual.setBounds(195, 172, 51, 29);
 					
 					btnSquareRoot = new JButton("\u221A");
 					btnSquareRoot.setBounds(261, 48, 64, 29);
@@ -120,8 +140,8 @@ public class CalculatorGUI implements ActionListener{
 					btnLog = new JButton("log");
 					btnLog.setBounds(261, 134, 64, 29);
 					
-					btnAc = new JButton("AC");
-					btnAc.setBounds(261, 172, 64, 29);
+					btnDot = new JButton(".");
+					btnDot.setBounds(261, 172, 64, 29);
 					
 					btnSin = new JButton("Sin");
 					btnSin.setBounds(340, 48, 64, 29);
@@ -132,8 +152,10 @@ public class CalculatorGUI implements ActionListener{
 					btnTan = new JButton("Tan");
 					btnTan.setBounds(340, 134, 64, 29);
 					
-					
+					btnAC = new JButton("AC");
+					btnAC.setBounds(340, 172, 64, 29);
 
+					MSCalc= new MethodsForSimpleCalculations();
 					
 				}
 	
@@ -159,20 +181,22 @@ public class CalculatorGUI implements ActionListener{
 		frame.getContentPane().add(button_6);
 		frame.getContentPane().add(button_7);
 		frame.getContentPane().add(button_8);
+
 		frame.getContentPane().add(btnminus);
 		frame.getContentPane().add(btnplus);
 		frame.getContentPane().add(btnMultiply);
 		frame.getContentPane().add(btnDivide);
 
 		frame.getContentPane().add(btnPi);
-		frame.getContentPane().add(btnClear);
+		frame.getContentPane().add(btnEqual);
 		frame.getContentPane().add(btnSquareRoot);
 		frame.getContentPane().add(btnSquare);
 		frame.getContentPane().add(btnLog);
-		frame.getContentPane().add(btnAc);
+		frame.getContentPane().add(btnDot);
 		frame.getContentPane().add(btnSin);
 		frame.getContentPane().add(btnCos);
 		frame.getContentPane().add(btnTan);
+		frame.getContentPane().add(btnAC);
 
 
 	}
@@ -193,22 +217,33 @@ public class CalculatorGUI implements ActionListener{
 		btnMultiply.addActionListener(this);
 		btnDivide.addActionListener(this);
 		btnPi.addActionListener(this);
-		btnClear.addActionListener(this);
+		btnEqual.addActionListener(this);
 		btnSquareRoot.addActionListener(this);
 		btnSquare.addActionListener(this);
 		btnLog.addActionListener(this);
-		btnAc.addActionListener(this);
+		btnDot.addActionListener(this);
 		btnSin.addActionListener(this);
 		btnCos.addActionListener(this);
 		btnTan.addActionListener(this);
-	
+		btnAC.addActionListener(this);
+
+	}
+
+	public void equalButtonMethod () {
+		mathInputString = textDisplay.getText();
+		try {
+			result = engine.eval(mathInputString);
+			textDisplay.setText(""+result);
+		} catch(Exception s) {
+			s.getMessage();
+		}
 	}
 
 
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
-		if (e.getSource()==btnClear){
+		if (e.getSource()==btnAC){
 			textDisplay.setText(null);
 		}			
 
@@ -222,8 +257,158 @@ public class CalculatorGUI implements ActionListener{
 			textDisplay.setText(EnterNumber);
 
 		}
+		
+
+		if (e.getSource()==button_1){
+			String EnterNumber = textDisplay.getText() +button_1.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+		
+		if (e.getSource()==button_2){
+			String EnterNumber = textDisplay.getText() +button_2.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+		if (e.getSource()==button_3){
+			String EnterNumber = textDisplay.getText() +button_3.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+		if (e.getSource()==button_4){
+			String EnterNumber = textDisplay.getText() +button_4.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+		if (e.getSource()==button_5){
+			String EnterNumber = textDisplay.getText() +button_5.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+		if (e.getSource()==button_6){
+			String EnterNumber = textDisplay.getText() +button_6.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+		if (e.getSource()==button_7){
+			String EnterNumber = textDisplay.getText() +button_7.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+
+		if (e.getSource()==button_8){
+			String EnterNumber = textDisplay.getText() +button_8.getText();
+			textDisplay.setText(EnterNumber);
+
+		}
+
+
+		if (e.getSource()==btnplus){
+			textDisplay.setText(textDisplay.getText()+"+");
+	}
+
+		if (e.getSource()==btnminus){
+			textDisplay.setText(textDisplay.getText()+"-");
+	}
+		if (e.getSource()==btnMultiply){
+			textDisplay.setText(textDisplay.getText()+"*");
+	}
+		if (e.getSource()==btnDivide){
+			textDisplay.setText(textDisplay.getText()+"/");
+	}
+		if (e.getSource() == btnDot) {
+			textDisplay.setText(textDisplay.getText()+".");
+		}
+
+
+		if (e.getSource()==btnEqual){
+			equalButtonMethod ();
+			}
+
+		if (e.getSource()== btnPi){
+			try {
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.pi(txt2dbl1);
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}		
+		} 
+		
+		if (e.getSource() == btnSquareRoot){
+			try {
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.squareRoot(txt2dbl1);
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}
+		}
 
 		
+		if (e.getSource() == btnSin){
+			try {
+				
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.sin(txt2dbl1);				
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}	
+		}
+
+		
+		if (e.getSource() == btnCos){
+			try {
+				
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.cos(txt2dbl1);				
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}	
+		}
+			
+		if (e.getSource() == btnTan){
+			try {
+				
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.tan(txt2dbl1);				
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}	
+		}
+
+		if (e.getSource() == btnLog){
+			try {
+				
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.log(txt2dbl1);				
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}	
+		}
+
+		if (e.getSource() == btnSquare){
+			try {
+				
+				String firstField = textDisplay.getText();
+				double txt2dbl1 = Double.parseDouble(firstField);
+				double res = mca.pow(txt2dbl1,txt2dbl1);				
+				textDisplay.setText(""+res);
+				} catch (Exception a) {
+					a.getMessage();
+				}	
+		}
+
 		
 	}
 }
