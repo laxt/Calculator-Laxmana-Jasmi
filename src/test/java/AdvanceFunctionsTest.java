fce632773bbf266765f4e40e0cffeddc8e8b5649
Jasmi <jasmine.shaheer@gmail.com>
2016-11-07 14:14:41 +0100

    Commit from jas-dev.I have almost completed with the testmethods for simple calculator. I also added pom file and generated test reports jacoco and surefire. Now java doc left

--- src/test/java/AdvanceFunctionsTest.java
+++ src/test/java/AdvanceFunctionsTest.java
@@ -7,271 +7,377 @@ import org.junit.Test;
 
 import otherclasses.MethodsForAdvancedCalculator;
 
+/**
+ * Calculator advanced functions class
+ * 
+ * @author Jasmi-Laxmana
+ * 
+ */
+
 public class AdvanceFunctionsTest {
 
 	MethodsForAdvancedCalculator calAdvance = new MethodsForAdvancedCalculator();
 	public static final Logger LOG = Logger.getLogger(MethodsForAdvancedCalculator.class.getName());
 	Random random = new Random();
-	
-      @Test 
-      public void testPow() {
-    	  double firstNumber = 0.0;
-			double secondNumber = 0.0;
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				firstNumber = -100.0 + random.nextDouble()*200;
-				secondNumber = -100.0 + random.nextDouble()*200;
-				result = Math.pow(firstNumber, secondNumber);
-				LOG.info("Testing the power method with the values " + firstNumber+" and "+ secondNumber +" = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.pow(firstNumber, secondNumber),result,0.001);
-			}
-      }
-      @Test 
-      public void testPow2() {
-    	  double firstNumber = 0.0;
-			double secondNumber = 0.0;
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				firstNumber = random.nextDouble()*200;
-				secondNumber =random.nextDouble()*200;
-				result = Math.pow(firstNumber, secondNumber);
-				LOG.info("Testing the power method with the values " + firstNumber+" and "+ secondNumber +" = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.pow(firstNumber, secondNumber),result,0.001);
-			}
-      }
-      @Test 
-      public void testPow3() {
-    	  double firstNumber = 0.0;
-			double secondNumber = 0.0;
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				firstNumber = random.nextDouble()*-200;
-				secondNumber =random.nextDouble()*200;
-				result = Math.pow(firstNumber, secondNumber);
-				LOG.info("Testing the power method with the values " + firstNumber+" and "+ secondNumber +" = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.pow(firstNumber, secondNumber),result,0.001);
-			}
-      }
-      
-      @Test 
-      public void testSin1() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*200;
-				
-				result = Math.sin(Number);
-				LOG.info("Testing the sin method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.sin(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testSin2() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*-200;
-				
-				result = Math.sin(Number);
-				LOG.info("Testing the sin method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.sin(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testSin3() {
-    	  double Number = 0.0;
-			double result = Math.sin(Number);
-				LOG.info("Testing the sin method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.sin(Number),result,0.001);
-			
-      }
-      
-      @Test 
-      public void testCos1() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*200;
-				
-				result = Math.cos(Number);
-				LOG.info("Testing the cos method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.cos(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testCos2() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*-200;
-				
-				result = Math.cos(Number);
-				LOG.info("Testing the cos method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.cos(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testCos3() {
-    	  double Number = 0.0;
-				
-				double result = Math.cos(Number);
-				LOG.info("Testing the cos method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.cos(Number),result,0.001);
-			
-      }
-      @Test 
-      public void testTan1() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*200;
-				
-				result = Math.tan(Number);
-				LOG.info("Testing the tan method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.tan(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testTan2() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*-200;
-				
-				result = Math.tan(Number);
-				LOG.info("Testing the tan method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.tan(Number),result,0.001);
-			}
-      }
-      public void testTan3() {
-    	  double Number = 0.0;
-				
-				double result = Math.tan(Number);
-				LOG.info("Testing the tan method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.tan(Number),result,0.001);
-			
-      }
-      @Test 
-      public void testSquareRoot1() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*200;
-				
-				result = Math.sqrt(Number);
-				LOG.info("Testing the sqrt method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.squareRoot(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testSquareRoot2() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*-200;
-				
-				result = Math.sqrt(Number);
-				LOG.info("Testing the sqrt method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.squareRoot(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testSquareRoot3() {
-    	  double Number = 0.0;
-			double result = Math.sqrt(Number);
-				LOG.info("Testing the sqrt method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.squareRoot(Number),result,0.001);
-			
-      }
-      @Test 
-      public void testExp() {
-    	 double Number =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*200;
-				
-				double result = Math.exp(Number);
-				LOG.info("Testing the exponential method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.exp(Number),result,0.001);
-			}
 
-			
-      }
-      
-      @Test 
-      public void testExp1() {
-    	 double Number =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*-200;
-				
-				double result = Math.exp(Number);
-				LOG.info("Testing the exponential method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.exp(Number),result,0.001);
-			}
+	/**
+	 * Testing power function using two random negative numbers with 50
+	 * iterations
+	 * 
+	 */
 
-			
-      }
+	@Test
+	public void testPow() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = -100.0 + random.nextDouble() * 200;
+			secondNumber = -100.0 + random.nextDouble() * 200;
+			result = Math.pow(firstNumber, secondNumber);
+			LOG.info("Testing the power method with the values " + firstNumber + " and " + secondNumber + " = "
+					+ result);
+			assertEquals("The result should be: " + result, calAdvance.pow(firstNumber, secondNumber), result, 0.001);
+		}
+	}
 
-      @Test 
-      public void testExp2() {
-    	 double Number =0.0;				
-				double result = Math.exp(Number);
-				LOG.info("Testing the exponential method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.exp(Number),result,0.001);
-			}
+	/**
+	 * Testing power function using two random positive numbers with 50
+	 * iterations
+	 * 
+	 * 
+	 */
+	@Test
+	public void testPow2() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = random.nextDouble() * 200;
+			secondNumber = random.nextDouble() * 200;
+			result = Math.pow(firstNumber, secondNumber);
+			LOG.info("Testing the power method with the values " + firstNumber + " and " + secondNumber + " = "
+					+ result);
+			assertEquals("The result should be: " + result, calAdvance.pow(firstNumber, secondNumber), result, 0.001);
+		}
+	}
 
-			
-      
+	/**
+	 * Testing power function using one random negative number and one random
+	 * positive number with 50 iterations
+	 */
+	@Test
+	public void testPow3() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = random.nextDouble() * -200;
+			secondNumber = random.nextDouble() * 200;
+			result = Math.pow(firstNumber, secondNumber);
+			LOG.info("Testing the power method with the values " + firstNumber + " and " + secondNumber + " = "
+					+ result);
+			assertEquals("The result should be: " + result, calAdvance.pow(firstNumber, secondNumber), result, 0.001);
+		}
+	}
 
-      @Test 
-      public void testLog1() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*200;
-				
-				result = Math.log(Number);
-				LOG.info("Testing the log method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.log(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testLog2() {
-    	  double Number = 0.0;
-			
-			double result =0.0;
-			for (int i = 0; i < 50; i++) {	
-				Number = random.nextDouble()*-200;
-				
-				result = Math.log(Number);
-				LOG.info("Testing the log method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.log(Number),result,0.001);
-			}
-      }
-      @Test 
-      public void testLog3() {
-    	  double Number = 0.0;
-				double result = Math.log(Number);
-				LOG.info("Testing the log method with the value " + Number+" and "+ " = " + result);
-				assertEquals("The result should be: "+ result, calAdvance.log(Number),result,0.001);
-			
-      }
-      
-      
-      
-      
-      
-      
-     
-      
-      
-      
+	/**
+	 * Testing Sin method with a random positive number with 50 iterations
+	 * 
+	 */
+
+	@Test
+	public void testSin1() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * 200;
+
+			result = Math.sin(Number);
+			LOG.info("Testing the sin method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.sin(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing Sin method with a random negative number with 50 iterations
+	 * 
+	 */
+
+	@Test
+	public void testSin2() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * -200;
+
+			result = Math.sin(Number);
+			LOG.info("Testing the sin method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.sin(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing Sin method with zero.
+	 * 
+	 */
+	@Test
+	public void testSin3() {
+		double Number = 0.0;
+		double result = Math.sin(Number);
+		LOG.info("Testing the sin method with the value " + Number + " and " + " = " + result);
+		assertEquals("The result should be: " + result, calAdvance.sin(Number), result, 0.001);
+
+	}
+
+	/**
+	 * Testing the Cos method with a random positive number with 50 iterations
+	 * 
+	 */
+
+	@Test
+	public void testCos1() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * 200;
+
+			result = Math.cos(Number);
+			LOG.info("Testing the cos method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.cos(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the Cos method with a random negative number with 50 iterations
+	 * 
+	 */
+	@Test
+	public void testCos2() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * -200;
+
+			result = Math.cos(Number);
+			LOG.info("Testing the cos method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.cos(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the Cos method with zero with 50 iterations
+	 */
+	@Test
+	public void testCos3() {
+		double Number = 0.0;
+
+		double result = Math.cos(Number);
+		LOG.info("Testing the cos method with the value " + Number + " and " + " = " + result);
+		assertEquals("The result should be: " + result, calAdvance.cos(Number), result, 0.001);
+
+	}
+
+	/**
+	 * Testing the Tan method with random positive numbers with 50 iterations
+	 * 
+	 */
+	@Test
+	public void testTan1() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * 200;
+
+			result = Math.tan(Number);
+			LOG.info("Testing the tan method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.tan(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the Tan method with random negative numbers with 50 iterations
+	 * 
+	 */
+	@Test
+	public void testTan2() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * -200;
+
+			result = Math.tan(Number);
+			LOG.info("Testing the tan method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.tan(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing tan method with zero
+	 */
+	@Test
+	public void testTan3() {
+		double Number = 0.0;
+
+		double result = Math.tan(Number);
+		LOG.info("Testing the tan method with the value " + Number + " and " + " = " + result);
+		assertEquals("The result should be: " + result, calAdvance.tan(Number), result, 0.001);
+
+	}
+
+	/**
+	 * Testing the squareroot method with random positive numbers
+	 * 
+	 */
+	@Test
+	public void testSquareRoot1() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * 200;
+
+			result = Math.sqrt(Number);
+			LOG.info("Testing the sqrt method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.squareRoot(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the Squareroot method with random negative numbers
+	 * 
+	 */
+	@Test
+	public void testSquareRoot2() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * -200;
+
+			result = Math.sqrt(Number);
+			LOG.info("Testing the sqrt method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.squareRoot(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the Squareroot method with zero
+	 * 
+	 */
+	@Test
+	public void testSquareRoot3() {
+		double Number = 0.0;
+		double result = Math.sqrt(Number);
+		LOG.info("Testing the sqrt method with the value " + Number + " and " + " = " + result);
+		assertEquals("The result should be: " + result, calAdvance.squareRoot(Number), result, 0.001);
+
+	}
+
+	/**
+	 * Testing exponential function with a random positive number with 50
+	 * iterations
+	 * 
+	 * 
+	 */
+	@Test
+	public void testExp() {
+		double Number = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * 200;
+
+			double result = Math.exp(Number);
+			LOG.info("Testing the exponential method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.exp(Number), result, 0.001);
+		}
+
+	}
+
+	/**
+	 * Testing exponential function with a random nagative number with 50
+	 * iterations
+	 * 
+	 */
+
+	@Test
+	public void testExp1() {
+		double Number = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * -200;
+
+			double result = Math.exp(Number);
+			LOG.info("Testing the exponential method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.exp(Number), result, 0.001);
+		}
+
+	}
+
+	/**
+	 * Testing exponential method with zero
+	 * 
+	 */
+
+	@Test
+	public void testExp2() {
+		double Number = 0.0;
+		double result = Math.exp(Number);
+		LOG.info("Testing the exponential method with the value " + Number + " and " + " = " + result);
+		assertEquals("The result should be: " + result, calAdvance.exp(Number), result, 0.001);
+	}
+
+	/**
+	 * Testing the log method with random positive numbers with 50 iterations
+	 * 
+	 */
+
+	@Test
+	public void testLog1() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * 200;
+
+			result = Math.log(Number);
+			LOG.info("Testing the log method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.log(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the log method with random negative numbers with 50 iterations
+	 * 
+	 */
+	@Test
+	public void testLog2() {
+		double Number = 0.0;
+
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			Number = random.nextDouble() * -200;
+
+			result = Math.log(Number);
+			LOG.info("Testing the log method with the value " + Number + " and " + " = " + result);
+			assertEquals("The result should be: " + result, calAdvance.log(Number), result, 0.001);
+		}
+	}
+
+	/**
+	 * Testing the log method with zero
+	 */
+	@Test
+	public void testLog3() {
+		double Number = 0.0;
+		double result = Math.log(Number);
+		LOG.info("Testing the log method with the value " + Number + " and " + " = " + result);
+		assertEquals("The result should be: " + result, calAdvance.log(Number), result, 0.001);
+
+	}
+
 }
--- src/test/java/SimpleFunctionsTest.java
+++ src/test/java/SimpleFunctionsTest.java
@@ -8,14 +8,24 @@ import org.junit.Test;
 import otherclasses.MethodsForSimpleCalculations;
 
 public class SimpleFunctionsTest {
+	/**
+	 * Simple calcultor test class
+	 * @author Jasmi-Laxmana
+	 * 
+	 * */
 
 
 	MethodsForSimpleCalculations calBasic = new MethodsForSimpleCalculations();
 	public static final Logger LOG = Logger.getLogger(MethodsForSimpleCalculations.class.getName());
 	Random random = new Random();
+	/**
+	 * Testing add method with 2 random negative numbers with 50 iterations
+	 * 
+	 * 
+	 * */
 
 	@Test
-	public void testAdd() {
+	public void testAdd1() {
 		double firstNumber = 0.0;
 		double secondNumber = 0.0;
 		double result = 0.0;
@@ -26,10 +36,47 @@ public class SimpleFunctionsTest {
 			LOG.info("Testing the add method with the values: "+ firstNumber+ " and " + secondNumber);
 			assertEquals("The result should be: "+result,calBasic.add(firstNumber, secondNumber), result, 0.0001);
 		}
+		
 	}
+	/**
+	 * Testing add method with 2 random positive numbers with 50 iterations
+	 * 
+	 * */
+	@Test
+	public void testAdd2() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result = 0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = random.nextDouble()*200;
+			secondNumber =random.nextDouble()*200;
+			result = firstNumber + secondNumber;	
+			LOG.info("Testing the add method with the values: "+ firstNumber+ " and " + secondNumber);
+			assertEquals("The result should be: "+result,calBasic.add(firstNumber, secondNumber), result, 0.0001);
+		}
+	}
+	/**
+	 * Test add method with zeros
+	 * 
+	 * */
+	@Test
+	public void testAdd3() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+			double result = firstNumber + secondNumber;	
+			LOG.info("Testing the add method with the values: "+ firstNumber+ " and " + secondNumber);
+			assertEquals("The result should be: "+result,calBasic.add(firstNumber, secondNumber), result, 0.0001);
+		
+	}
+	/**
+	 * Testing Subtract method with 2 random negative methods
+	 * 
+	 * 
+	 * */
+
 
 	@Test
-	public void testSubtract() {
+	public void testSubtract1() {
 		double firstNumber = 0.0;
 		double secondNumber = 0.0;
 		double result =0.0;
@@ -41,9 +88,45 @@ public class SimpleFunctionsTest {
 			assertEquals("The result should be: "+ result, calBasic.subtract(firstNumber, secondNumber),result,0.0001);
 		}
 	}
+	/**
+	 * Testing subtract method with 2 random positive numbers
+	 * 
+	 * */
+	@Test
+	public void testSubtract2() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result =0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = random.nextDouble()*200;
+			secondNumber =random.nextDouble()*200;
+			result = firstNumber - secondNumber;
+			LOG.info("Testing the subtract method with the values " + firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result, calBasic.subtract(firstNumber, secondNumber),result,0.0001);
+		}
+	}
+	/**
+	 * Testing Subtract method with zeros
+	 * 
+	 * 
+	 * */
+	@Test
+	public void testSubtract3() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+			double result = firstNumber - secondNumber;	
+			LOG.info("Testing the add method with the values: "+ firstNumber+ " and " + secondNumber);
+			assertEquals("The result should be: "+result,calBasic.subtract(firstNumber, secondNumber), result, 0.0001);
+		
+	}
+	/**
+	 * Testing multiply method with 2 random negative numbers with 50 iterations
+	 * 
+	 * */
+
 
 	@Test
-	public void testMultiply() {
+	public void testMultiply1() {
 		double firstNumber = 0.0;
 		double secondNumber = 0.0;
 		double result =0.0;
@@ -55,9 +138,44 @@ public class SimpleFunctionsTest {
 			assertEquals("The result should be: "+ result, calBasic.multiply(firstNumber, secondNumber),result,0.0001);
 		}
 	}
+	/**
+	 * Testing multiply method with 2 random positive numbers with 50 iterations
+	 * 
+	 * */
+	@Test
+	public void testMultiply2() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result =0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = random.nextDouble()*200;
+			secondNumber =random.nextDouble()*200;
+			result = firstNumber * secondNumber;
+			LOG.info("Testing the multiply method with the values " + firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result, calBasic.multiply(firstNumber, secondNumber),result,0.0001);
+		}
+	}
+	/**
+	 * Testing the multiply with zeros.
+	 * 
+	 * */
+	@Test
+	public void testMultiply3() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+			double result = firstNumber * secondNumber;	
+			LOG.info("Testing the add method with the values: "+ firstNumber+ " and " + secondNumber);
+			assertEquals("The result should be: "+result,calBasic.multiply(firstNumber, secondNumber), result, 0.0001);
+		
+	}
+/**
+ * Testing the divide method with random negative numbers with 50 iterations. Testing also division by zero
+ * 
+ * */
+
 
 	@Test
-	public void testDivide() {
+	public void testDivide1() {
 		double firstNumber = 0.0;
 		double secondNumber = 0.0;
 		double result =0.0;
@@ -68,10 +186,45 @@ public class SimpleFunctionsTest {
 			LOG.info("Testing the divide method with the values " + firstNumber+" and "+ secondNumber);
 			assertEquals("The result should be: "+ result, calBasic.divide(firstNumber, secondNumber),result,0.0001);
 		}
-		secondNumber = 0.0;
-		LOG.info("Testing the divide method with values " + firstNumber + " and 0.0");
-		assertTrue(calBasic.divide(firstNumber, secondNumber) == -1111.1111);
+		
 	}
+	
+	/**
+	 * Testing the divide method with 2 random positive numbers with 50 iterations
+	 * 
+	 * */
+	@Test
+	public void testDivide2() {
+		double firstNumber = 0.0;
+		double secondNumber = 0.0;
+		double result =0.0;
+		for (int i = 0; i < 50; i++) {
+			firstNumber = random.nextDouble()*200;
+			secondNumber =random.nextDouble()*200;
+			result = firstNumber / secondNumber;
+			LOG.info("Testing the divide method with the values " + firstNumber+" and "+ secondNumber);
+			assertEquals("The result should be: "+ result, calBasic.divide(firstNumber, secondNumber),result,0.0001);
+		}
+	}
+	/**
+	 * Testing divide by zero
+	 * 
+	 * */
+	@Test
+	public void testDivide3() {
+		double firstNumber = 0.0;
+		double secondNumber= 0.0;
+		double result =-0.123456;
+		result = firstNumber / secondNumber;
+		LOG.info("Testing the divide method with the values " + firstNumber+" and "+ secondNumber);
+		assertEquals(Math.round(calBasic.divide(firstNumber, secondNumber)),Math.round(result));
+		
+		
+			
+		
+}
+	
+	
 }
 
 
