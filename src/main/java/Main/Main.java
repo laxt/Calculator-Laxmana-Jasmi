44e2d9533296db82b984e79618aac309af19de55
Jasmi <jasmine.shaheer@gmail.com>
2016-11-08 22:57:50 +0100

    Commit from jas-dev.Generated test reports.

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -7,6 +7,11 @@
 package Main;
 
 import gui.CalculatorGUI;
+/**
+ *Main class for implementing Scientific Calculator
+ *@author Jasmi-Laxmana  
+ * 
+ * */
 
 public class Main {
 	public static void main(String[] args) {
